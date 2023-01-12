fb8ba6834f49b81062584397285ce0d86d816301
Chad Woolley <cwoolley@gitlab.com>
2023-01-12 15:05:32 -0700

    WIP - BROKEN - trying to hook process into netlify build plugin
    - But 'npx run netlify build' does nothing
    - Try with a simpler command like just 'ls' and see if that works

--- netlify-plugins/test-unit/index.js
+++ netlify-plugins/test-unit/index.js
@@ -1,7 +1,23 @@
 // index.js
+const { exec } = require('child_process');
 
 module.exports = {
   onPostBuild: () => {
-    console.log("Hello world!");
+    console.log("About to run exec")
+    exec('npm run test-unit', (error, stdout, stderr) => {
+      console.log("Inside exec callback")
+      if (error) {
+        console.error(`error: ${error.message}`);
+        return;
+      }
+
+      if (stderr) {
+        console.error(`stderr: ${stderr}`);
+        return;
+      }
+
+      console.log(`stdout:\n${stdout}`);
+    });
+    console.log("After run exec")
   },
 }
