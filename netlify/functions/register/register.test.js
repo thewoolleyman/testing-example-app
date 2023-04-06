e8715ca7aeafab922f94d5d149c3693be85ef5f4
Chad Woolley <cwoolley@gitlab.com>
2023-04-06 13:40:13 -0700

    added contexts for success/fail to register test

--- netlify/functions/register/register.test.js
+++ netlify/functions/register/register.test.js
@@ -1,8 +1,14 @@
 import {handler} from './register'
 import {describe, it, expect} from 'vitest'
+
 describe('testing register function', () => {
-  it('returns hello + given email', async () => {
-    // TODO: Shape of event argument and response must be correct for a netlify serverless event 
-    expect(await handler({})).eq('Hello test.example.com!')
+  describe('when successful', () => {
+    it('returns hello + given email', async () => {
+      // TODO: Shape of event argument and response must be correct for a netlify serverless event 
+      expect(await handler({})).eq('Hello test.example.com!')
+    })
+  })
+  describe('when failing', () => {
+    it.skip('handles error')
   })
 })
