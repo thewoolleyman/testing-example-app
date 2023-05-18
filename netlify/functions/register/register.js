84b7775cc051a57f40194e43f05568ee9db75c06
Chad Woolley <cwoolley@gitlab.com>
2023-05-18 14:01:17 -0700

    Introduce queryStringParameters to event payload in test

--- netlify/functions/register/register.js
+++ netlify/functions/register/register.js
@@ -1,12 +1,14 @@
 const handler = async (event) => {
-  if (!event.name) {
+  const name = event.queryStringParameters.name;
+  if (!name) {
     return {
       statusCode: 400,
+      body: 'Name is required',
     }
   }
   return {
     statusCode: 200,
-    body: `Hello ${event.name}`
+    body: `Hello ${name}`
   }
 }
 
--- netlify/functions/register/register.test.js
+++ netlify/functions/register/register.test.js
@@ -5,23 +5,30 @@ describe('testing register handler function', () => {
   describe('when successful', () => {
     it('returns hello + given name as body property in the response', async () => {
       const name = 'robot'
-      const event = {name: name}
+      const event = {queryStringParameters: {name: name}}
       const actualResponse = await handler(event);
       expect(actualResponse.body).eql(`Hello ${name}`)
     })
 
     it('returns 200 as the statusCode property in the response', async () => {
-      const event = {name: 'blah'};
+      const event = {queryStringParameters: {name: 'blah'}};
       const actualResponse = await handler(event);
       expect(actualResponse.statusCode).eql(200)
     })
   })
+
   describe('when failing', () => {
     describe('when name is not provided', () => {
       it('returns 400 as the statusCode property in the response', async () => {
-        const event = {}
+        const event = {queryStringParameters:{}}
         const actualResponse = await handler(event);
         expect(actualResponse.statusCode).eql(400)
+      })
+
+      it('returns "Name is required" in the response', async () => {
+        const event = {queryStringParameters: {}}
+        const actualResponse = await handler(event);
+        expect(actualResponse.body).eql('Name is required')
       })
     })
   })
